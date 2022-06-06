b205bd9eb6f28addbb980b39672efe5ba8c553f4
EsauM10 <esau2096@gmail.com>
2022-06-06 17:01:52 -0300

    Add Makefile and installation instructions

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name="fogbed",
     version="1.0.0",
-    packages=find_packages(),
     description='Containernet fork that add Fogbed support.',
     long_description='Containernet fork that add Fogbed support.',
     keywords=['networking', 'emulator', 'protocol', 'Internet', 'OpenFlow', 'SDN', 'fog'],
-    include_package_data=True,
     url='https://github.com/EsauM10/fogbed',
     author='Esaú Mascarenhas',
     author_email='esaumasc@gmail.com',
@@ -16,5 +14,8 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Topic :: System :: Emulators'
-    ]
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    requires=['Pyro4']
 )
