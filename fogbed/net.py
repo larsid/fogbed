ad128cd9d793dc188c2ea51178a2bc0738056bf2
EsauM10 <esau2096@gmail.com>
2022-08-20 16:03:50 -0300

    refactor: add return type

--- fogbed/net.py
+++ fogbed/net.py
@@ -1,4 +1,5 @@
 from mininet.net import Containernet
+from mininet.link import Link
 
 from fogbed.node.instance import VirtualInstance
 
@@ -7,7 +8,7 @@ class Fogbed(Containernet):
         super().__init__(**params)
     
     
-    def addLink(self, node1, node2, **params) -> int:
+    def addLink(self, node1, node2, **params) -> Link:
         assert node1 is not None
         assert node2 is not None
         
@@ -17,14 +18,14 @@ class Fogbed(Containernet):
         return Containernet.addLink(self, node1, node2, **params)
 
 
-    def removeLink(self, node1, node2, **params) -> int:
+    def removeLink(self, node1, node2, **params):
         assert node1 is not None
         assert node2 is not None
         
         if(isinstance(node1, VirtualInstance)): node1 = node1.switch
         if(isinstance(node2, VirtualInstance)): node2 = node2.switch
 
-        return Containernet.addLink(self, node1, node2, **params)
+        return Containernet.removeLink(self, node1, node2, **params)
 
 
     def start(self):
