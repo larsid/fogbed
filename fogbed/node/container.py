721bd1b5e4168c5f8df0af5f7fdaf926ee564a3c
EsauM10 <esau2096@gmail.com>
2022-08-20 20:11:26 -0300

    feat: create properties to get cpu and memory limits

--- fogbed/node/container.py
+++ fogbed/node/container.py
@@ -14,6 +14,21 @@ class Container:
         self.params['mem_limit'] = memory_limit
 
     @property
+    def cpu_period(self) -> int:
+        cpu_period = self.params.get('cpu_period')
+        return -1 if(cpu_period is None) else cpu_period
+
+    @property
+    def cpu_quota(self) -> int:
+        cpu_quota = self.params.get('cpu_quota')
+        return -1 if(cpu_quota is None) else cpu_quota
+
+    @property
+    def mem_limit(self) -> int:
+        mem_limit = self.params.get('mem_limit')
+        return -1 if(mem_limit is None) else mem_limit
+
+    @property
     def resources(self) -> 'Dict[str, Any] | None':
         return self.params.get('resources')
     
@@ -28,6 +43,6 @@ class Container:
         return 0 if(resources is None) else resources['mu']
     
     def __repr__(self) -> str:
-        cpu_quota  = self.params.get('cpu_quota')
-        cpu_period = self.params.get('cpu_period')
+        cpu_quota  = self.cpu_quota
+        cpu_period = self.cpu_period
         return f'Container(name={self.name}, cpu_quota={cpu_quota}, cpu_period={cpu_period})'
