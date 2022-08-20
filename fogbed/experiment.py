a09e3005ef688010bd5c516b60dfd899058545ad
EsauM10 <esau2096@gmail.com>
2022-08-20 16:10:13 -0300

    feat: add support for creating experiments

---
+++ fogbed/experiment.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from fogbed.net import Fogbed
+from fogbed.topo import FogTopo
+
+from mininet.cli import CLI
+from mininet.node import Controller, Docker, UserSwitch
+
+
+class FogbedExperiment:
+    def __init__(self, topology: FogTopo) -> None:
+        self.topology = topology
+        self.topology.create()
+        self.net = Fogbed(topo=topology, controller=Controller, switch=UserSwitch)
+    
+    def get_all_docker_names(self) -> List[str]:
+        return [container.name for container in self.net.hosts]
+    
+    def get_node(self, name: str) -> Docker:
+        if(not name in self.get_all_docker_names()):
+            raise Exception(f'Container {name} not found.')
+        return self.net[name]
+
+    def start_cli(self):
+        CLI(self.net)
+
+    def start(self):
+        self.net.start()
+
+    def stop(self):
+        self.net.stop()
