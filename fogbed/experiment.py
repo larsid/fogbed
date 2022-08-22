b4edf271316b0cdbb10659089e0216f5040aff3b
EsauM10 <esau2096@gmail.com>
2022-08-22 13:42:53 -0300

    refactor: set default arguments to run a experiment

--- fogbed/experiment.py
+++ fogbed/experiment.py
@@ -1,17 +1,17 @@
-from typing import List
+from typing import List, Type
 
 from fogbed.net import Fogbed
 from fogbed.topo import FogTopo
 
 from mininet.cli import CLI
-from mininet.node import Controller, Docker, UserSwitch
+from mininet.node import Controller, Docker, Switch, UserSwitch
 
 
 class FogbedExperiment:
-    def __init__(self, topology: FogTopo) -> None:
+    def __init__(self, topology: FogTopo, controller=Controller, switch: Type[Switch]=UserSwitch) -> None:
         self.topology = topology
         self.topology.create()
-        self.net = Fogbed(topo=topology, controller=Controller, switch=UserSwitch)
+        self.net = Fogbed(topo=topology, controller=controller, switch=switch)
     
     def get_all_docker_names(self) -> List[str]:
         return [container.name for container in self.net.hosts]
