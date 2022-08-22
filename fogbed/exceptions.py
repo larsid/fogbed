ef9f5733b1fdfbeafb251816ef0cf545d1393c73
EsauM10 <esau2096@gmail.com>
2022-08-22 14:58:45 -0300

    refactor: add module exceptions

---
+++ fogbed/exceptions.py
@@ -0,0 +1,14 @@
+class ContainerAlreadyExists(Exception):
+    pass
+
+class ContainerNotFound(Exception):
+    pass
+
+class NotEnoughResourcesAvailable(Exception):
+    pass
+
+class ResourceModelNotFound(Exception):
+    pass
+
+class VirtualInstanceAlreadyExists(Exception):
+    pass
