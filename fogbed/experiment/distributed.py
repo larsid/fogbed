277e5dcecd1dbd869f41ab96a8601c7d218ed961
EsauM10 <esau2096@gmail.com>
2022-08-27 16:03:37 -0300

    refactor: move helper methods to class FogTopo

--- fogbed/experiment/distributed.py
+++ fogbed/experiment/distributed.py
@@ -1,38 +1,20 @@
-from typing import List
-
 from MaxiNet.Frontend.maxinet import Cluster, Experiment
 from MaxiNet.Frontend.maxinet.node import DockerWrapper
 from MaxiNet.Frontend.partitioner import Clustering
 
-from mininet.topo import Topo
-
 from fogbed.emulation import EmulationCore
 from fogbed.node.instance import VirtualInstance
+from fogbed.topo import FogTopo
 
 class FogbedDistributedExperiment:
-    def __init__(self, topology: Topo) -> None:
+    def __init__(self, topology: FogTopo) -> None:
         clustering      = Clustering(
-            topologies=self.get_topologies(),
-            tunnels=self.get_tunnels(topology)
+            topologies=topology.get_topologies(),
+            tunnels=topology.get_tunnels()
         )
         cluster         = Cluster()
         self.experiment = Experiment(cluster, clustering)
     
-
-    def get_topologies(self) -> List[Topo]:
-        datacenters = EmulationCore.virtual_instances()
-        
-        return [
-            datacenter.create_topology()
-            for datacenter in datacenters.values()
-        ]
-    
-
-    def get_tunnels(self, topology: Topo) -> List:
-        return [
-            [s1, s2, topology.linkInfo(s1, s2)]
-            for s1, s2 in topology.links()  # type: ignore
-        ]
 
     def get_node(self, name: str) -> DockerWrapper:
         return self.experiment.get_docker(name)
--- fogbed/topo.py
+++ fogbed/topo.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fogbed.emulation import EmulationCore
 from fogbed.exceptions import VirtualInstanceAlreadyExists
 from fogbed.node.instance import VirtualInstance
@@ -37,3 +39,17 @@ class FogTopo(Topo):
                 self.addLink(container.name, datacenter.switch)
     
 
+    def get_topologies(self) -> List[Topo]:
+        datacenters = EmulationCore.virtual_instances()
+        
+        return [
+            datacenter.create_topology()
+            for datacenter in datacenters.values()
+        ]
+    
+
+    def get_tunnels(self) -> List:
+        return [
+            [s1, s2, self.linkInfo(s1, s2)]
+            for s1, s2 in self.links()  # type: ignore
+        ]
