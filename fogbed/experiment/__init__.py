38e34bfb55ce34e959b49233385f5d1a479800ff
EsauM10 <esau2096@gmail.com>
2022-10-16 21:58:36 -0300

    feat: add abstract class Experiment

--- fogbed/experiment/__init__.py
+++ fogbed/experiment/__init__.py
@@ -1,2 +1,47 @@
-from fogbed.experiment.local import FogbedExperiment
-from fogbed.experiment.distributed import FogbedDistributedExperiment
+from abc import ABC, abstractmethod
+from typing import Container, List
+
+from fogbed.node.instance import VirtualInstance
+from fogbed.resources import ResourceModel
+
+
+class Experiment(ABC):
+    @abstractmethod
+    def add_link(self, node1: VirtualInstance, node2: VirtualInstance, **params):
+        pass
+
+    @abstractmethod
+    def add_virtual_instance(self, name: str, resource_model: ResourceModel) -> VirtualInstance:
+        pass
+    
+    @abstractmethod
+    def add_docker(self, container: Container, datacenter: VirtualInstance):
+        pass
+
+    @abstractmethod
+    def get_docker(self, name: str) -> Container:
+        pass
+    
+    @abstractmethod
+    def get_containers(self) -> List[Container]:
+        pass
+
+    @abstractmethod
+    def get_virtual_instances(self) -> List[VirtualInstance]:
+        pass        
+
+    @abstractmethod
+    def remove_docker(self, name: str):            
+        pass
+
+    @abstractmethod
+    def start_cli(self):
+        pass
+
+    @abstractmethod
+    def start(self):
+        pass
+    
+    @abstractmethod
+    def stop(self):
+        pass
