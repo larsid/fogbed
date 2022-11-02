20ca9b44ae5b7a1b5551c7acfcac0c62b57dfbc1
EsauM10 <esau2096@gmail.com>
2022-11-02 13:52:45 -0300

    feat: add RemoteDocker service

---
+++ fogbed/node/services/remote_docker.py
@@ -0,0 +1,25 @@
+
+from fogbed.node.services import DockerService
+from clusternet.client.container import RemoteContainer
+
+class RemoteDocker(DockerService):
+    def __init__(self, name: str, url: str) -> None:
+        self.docker = RemoteContainer(name, url)
+
+    def get_ip(self) -> str:
+        return self.docker.get_ip()
+    
+    def run_command(self, command: str) -> str:
+        return self.docker.cmd(command)
+    
+    def update_cpu(self, cpu_quota: int, cpu_period: int):
+        return self.docker.update_cpu(cpu_quota, cpu_period)
+    
+    def update_memory(self, memory_in_bytes: int):
+        return self.docker.update_memory(memory_in_bytes)
+    
+    def start(self):
+        pass
+    
+    def stop(self):
+        pass
