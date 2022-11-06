c067a044a111f03becdac1b649527f71e97fd90a
EsauM10 <esau2096@gmail.com>
2022-11-05 23:30:00 -0300

    refactor: implement all abstract methods

--- fogbed/experiment/distributed.py
+++ fogbed/experiment/distributed.py
@@ -1,55 +1,106 @@
-from typing import Type
-from MaxiNet.Frontend.maxinet import Cluster, Experiment
-from MaxiNet.Frontend.maxinet.node import DockerWrapper
-from MaxiNet.Frontend.partitioner import Clustering
+from typing import Dict, List
 
-from fogbed.emulation import EmulationCore
+from fogbed.emulation import Services
+from fogbed.exceptions import ContainerNotFound, NotEnoughResourcesAvailable
+from fogbed.experiment import Experiment
+from fogbed.experiment.helpers import (
+    verify_if_container_ip_exists, 
+    verify_if_container_name_exists,
+    verify_if_datacenter_exists
+)
 from fogbed.node.instance import VirtualInstance
 from fogbed.node.container import Container
-from fogbed.topo import FogTopo
+from fogbed.node.services.remote_docker import RemoteDocker
+from fogbed.node.worker import FogWorker
+from fogbed.resources import ResourceModel
 
-from mininet.node import Docker, OVSSwitch, Switch
+from mininet.log import info
+
+class FogbedDistributedExperiment(Experiment):
+    def __init__(self, controller_ip: str, controller_port: int) -> None:
+        self.controller_ip   = controller_ip
+        self.controller_port = controller_port
+        self.workers: Dict[str, FogWorker] = {}
+        self.is_running = False
 
 
-class FogbedDistributedExperiment:
-    def __init__(self, topology: FogTopo, switch: Type[Switch]=OVSSwitch) -> None:
-        clustering      = Clustering(
-            topologies=topology.get_topologies(),
-            tunnels=topology.get_tunnels()
-        )
-        cluster         = Cluster()
-        self.experiment = Experiment(cluster, clustering, switch=switch)
+    def add_docker(self, container: Container, datacenter: VirtualInstance):
+        verify_if_container_name_exists(container.name)
+        verify_if_container_ip_exists(container.ip)
 
-    
-    def add_docker(self, container: Container, datacenter: VirtualInstance): 
-        worker_id = self.experiment.get_worker_id_by_nodename(datacenter.switch)
-        
         try:
             datacenter.create_container(container)
-            self.experiment.addHost(container.name, cls=Docker, wid=worker_id, **container.params)
-            self.experiment.addLink(container.name, datacenter.switch, autoconf=True)
-        except Exception as ex:
-            print(ex)
-        else:
-            self.update_containers(datacenter)
+
+            if(self.is_running):
+                worker = self._get_worker_by_datacenter(datacenter)
+                worker.net.add_docker(container.name, **container.params)
+                worker.net.add_link(container.name, datacenter.switch)
+                worker.net.config_default(container.name)
+                service = RemoteDocker(container.name, worker.net.url)
+                container.set_docker(service)
+
+        except NotEnoughResourcesAvailable:
+            info(f'{container.name}: Allocation of container was blocked by resource model.\n\n')
+
+              
+
+    def add_link(self, worker1: FogWorker, worker2: FogWorker): 
+        worker1.add_tunnel(worker2.ip)
+        worker2.add_tunnel(worker1.ip)
+        
+
+    def add_worker(self, ip: str) -> FogWorker:
+        if(ip in self.workers):
+            raise Exception(f'Already exist a worker with ip={ip}')
+
+        worker = FogWorker(ip=ip)
+        self.workers[worker.ip] = worker
+        return worker
+    
+
+    def add_virtual_instance(self, name: str, resource_model: ResourceModel) -> VirtualInstance:
+        verify_if_datacenter_exists(name)
+        datacenter = VirtualInstance(name)
+        datacenter.assignResourceModel(resource_model)
+        Services.add_virtual_instance(datacenter)
+        return datacenter
 
 
-    def get_node(self, name: str) -> DockerWrapper:
-        return self.experiment.get_docker(name)
+    def get_containers(self) -> List[Container]:
+        return Services.get_all_containers()
+
+
+    def get_docker(self, name: str) -> Container:
+        container = Services.get_container_by_name(name)
+        
+        if(container is None):
+            raise ContainerNotFound(f'Container {name} not found.')
+        return container
+
+
+    def get_virtual_instances(self) -> List[VirtualInstance]:
+        return list(Services.virtual_instances().values())
+
+    def _get_worker_by_datacenter(self, datacenter: VirtualInstance) -> FogWorker:
+        return self.workers[datacenter.get_ip()]
+
+    def remove_docker(self, name: str):
+        datacenter = Services.get_virtual_instance_by_container(name)
+        datacenter.remove_container(name)
+
+        if(self.is_running):
+            worker = self._get_worker_by_datacenter(datacenter)
+            worker.net.remove_link(name, datacenter.switch)
+            worker.net.remove_docker(name)
+
 
     def start(self):
-        self.experiment.setup()
-        for datacenter in EmulationCore.virtual_instances().values():
-            self.update_containers(datacenter)
-
-    def update_containers(self, datacenter: VirtualInstance):
-        for container in datacenter:
-            node = self.get_node(container.name)
-            node.updateCpuLimit(container.cpu_quota, container.cpu_period)
-            node.updateMemoryLimit(container.mem_limit)
-
-    def start_cli(self):
-        self.experiment.CLI(locals(), globals())
+        for worker in self.workers.values():
+            worker.start(self.controller_ip, self.controller_port)
+        self.is_running = True
 
     def stop(self):
-        self.experiment.stop()
+        for worker in self.workers.values():
+            if(worker.is_running):
+                worker.stop()
+        self.is_running = False
