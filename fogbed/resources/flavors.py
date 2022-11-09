9790390f718af1c3dfcbf88ef564a070e1b3e790
EsauM10 <esau2096@gmail.com>
2022-11-09 17:15:07 -0300

    feat: add module to provide default hardware resources

---
+++ fogbed/resources/flavors.py
@@ -0,0 +1,12 @@
+class HardwareResources:
+    def __init__(self, cu: float, mu: int) -> None:
+        self.compute_units = cu
+        self.memory_units  = mu
+
+
+class Resources:
+    TINY   = HardwareResources(cu=0.5,  mu=32)
+    SMALL  = HardwareResources(cu=1.0,  mu=128)
+    MEDIUM = HardwareResources(cu=4.0,  mu=256)
+    LARGE  = HardwareResources(cu=8.0,  mu=512)
+    XLARGE = HardwareResources(cu=16.0, mu=1024)
