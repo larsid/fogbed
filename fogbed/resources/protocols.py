0d71428882ddee6203b4b91f1e081271591058b5
EsauM10 <esau2096@gmail.com>
2022-12-18 21:50:02 -0300

    refactor: remove predefined resources dict

--- fogbed/resources/protocols.py
+++ fogbed/resources/protocols.py
@@ -3,22 +3,7 @@ from abc import ABC, abstractmethod
 from fogbed.node.container import Container
 
 
-PREDEFINED_RESOURCES = dict(
-    tiny   = {'cu': 0.5,  'mu': 32},
-    small  = {'cu': 1.0,  'mu': 128},
-    medium = {'cu': 4.0,  'mu': 256},
-    large  = {'cu': 8.0,  'mu': 512},
-    xlarge = {'cu': 16.0, 'mu': 1024},
-)
-
-
 class ResourceModel(ABC):
-    TINY   = PREDEFINED_RESOURCES['tiny']
-    SMALL  = PREDEFINED_RESOURCES['small']
-    MEDIUM = PREDEFINED_RESOURCES['medium']
-    LARGE  = PREDEFINED_RESOURCES['large']
-    XLARGE = PREDEFINED_RESOURCES['xlarge']
-
     def __init__(self, max_cu: float, max_mu: int) -> None:
         self.max_cu = max_cu
         self.max_mu = max_mu
